65c61c5c2cd59a2793ed35a69c702536b45eb0ee
Senior Dev <fightingmonkey1@gmail.com>
2021-11-21 15:16:53 +0900

    add empty component

--- apps/weather-forecast/src/app/app.module.ts
+++ apps/weather-forecast/src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppComponent } from './app.component';
 import { CityCardComponent } from './city-card/city-card.component';
 import { ClockComponent } from './clock/clock.component';
 import { DateComponent } from './date/date.component';
+import { ForecastComponent } from './forecast/forecast.component';
+import { HeaderComponent } from './header/header.component';
+import { LoaderComponent } from './loader/loader.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
-	declarations: [AppComponent, CityCardComponent, ClockComponent, DateComponent],
+	declarations: [AppComponent, CityCardComponent, ClockComponent, DateComponent, ForecastComponent, HeaderComponent, LoaderComponent, NotFoundComponent, SearchBarComponent, WeatherComponent],
 	imports: [BrowserModule],
 	providers: [],
 	bootstrap: [AppComponent],
---
+++ apps/weather-forecast/src/app/forecast/forecast.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bp-forecast',
+  templateUrl: './forecast.component.html',
+  styleUrls: ['./forecast.component.scss']
+})
+export class ForecastComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
